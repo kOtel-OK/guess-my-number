0e9dc6543165dc90f8bff4d51efcd318afa019da
admin <rkotelnykov@gmail.com>
2022-03-31 17:47:19 +0300

    Manipulating CSS styles

--- script.js
+++ script.js
@@ -2,17 +2,27 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
 let score = 20; //state variable
 
-document.querySelector('.number').textContent = secretNumber;
+// document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+
+    //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.number').style.width = '30rem';
+
+    //When number too hight
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too hight!';
@@ -22,6 +32,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'GAME OVER!';
       document.querySelector('.score').textContent = 0;
     }
+    //When number to low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!';
