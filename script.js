27a871436c1f16810428d599c2e57af915e650ab
admin <rkotelnykov@gmail.com>
2022-03-31 16:37:44 +0300

    Score counter added

--- script.js
+++ script.js
@@ -8,24 +8,28 @@ document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  if (score > 1) {
-    if (!guess) {
-      document.querySelector('.message').textContent = 'No number!';
-    } else if (guess === secretNumber) {
-      document.querySelector('.message').textContent = 'Correct Number!';
-    } else if (guess > secretNumber) {
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
       document.querySelector('.message').textContent = 'Too hight!';
       score--;
       document.querySelector('.score').textContent = score;
-    } else if (guess < secretNumber) {
+    } else {
+      document.querySelector('.message').textContent = 'GAME OVER!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!';
       score--;
       document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'GAME OVER!';
+      document.querySelector('.score').textContent = 0;
     }
-  } else {
-    score--;
-    document.querySelector('.score').textContent = score;
-
-    document.querySelector('.message').textContent = 'GAME OVER!';
   }
 });
